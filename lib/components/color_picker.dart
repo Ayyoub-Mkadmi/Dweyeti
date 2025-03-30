d19acee88e21f2c8ef73baf46be9d0e43289cfbc
Ayyoub <ayyoub.mkadmi3@gmail.com>
2025-03-30 14:44:49 +0100

    tojrab

--- lib/components/color_picker.dart
+++ lib/components/color_picker.dart
@@ -41,7 +41,7 @@ class ColorPicker extends StatelessWidget {
             Row(
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
-                _buildColorOption(const Color.fromARGB(255, 42, 94, 131)),
+                _buildColorOption(const Color.fromARGB(255, 237, 101, 33)),
                 SizedBox(width: 16),
                 _buildColorOption(Colors.purple),
                 SizedBox(width: 16),
